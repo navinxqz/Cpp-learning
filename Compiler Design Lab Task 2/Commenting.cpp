b84917b4b33027869bcc4eb85b430e9e6cc8e271
navin-nawshin <navinmdnawshin@gmail.com>
2024-11-01 19:20:24 +0600

    tasks .

--- Compiler Design Lab Task 2/Commenting.cpp
+++ Compiler Design Lab Task 2/Commenting.cpp
@@ -8,12 +8,16 @@ int main()
     cout<<"Input line: ";
     getline(cin,s);
 
-    if(s.substr(0,2) == "//")
+    if(s.substr(0,2) == "//")   //at least 2 characters
         {
             cout<<"This is a single line comment!\n";
-        }else if(s.substr(0,2) == "/*" && s.size() > 4 && s.substr(s.size()-2 , 2) == "*/")
-        {
-            cout<<"This is a multi line comment!\n";
+        }else if(s.length() >= 4){  //at least 4 characters
+            if(s[0]== '/' && s[1] == '*' && s[s.length()-2] == '*' && s[s.length()-1] == '/')
+            {
+                cout<<"This is a multiline comment!\n";
+            }else{
+                cout<<"This is not a comment\n";
+            }
         }else
         {
             cout<<"This is not a comment\n";
