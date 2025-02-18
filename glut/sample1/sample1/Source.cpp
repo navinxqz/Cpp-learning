74add4a86c72e2046d260d4e5f31f68c7031ef5c
Navin <navinmdnawshin@gmail.com>
2025-02-19 01:24:27 +0600

    earth and moon renderScene added

--- glut/sample1/sample1/Source.cpp
+++ glut/sample1/sample1/Source.cpp
@@ -18,8 +18,45 @@ void ChangeSize(GLsizei w, GLsizei h) {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
+void RenderScene() {
+	static float fMoonRot = 0.0f;
+	static float fEarthRot = 0.0f;
 
-int main() {
-	
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glMatrixMode(GL_MODELVIEW);
+	glPushMatrix();
+
+	glTranslatef(0.0f, 0.0f, -300.0f);
+	glColor3b(255, 255, 0);
+	glutSolidSphere(15.0f, 15, 15);
+	glEnable(GL_LIGHTING);
+
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+	glRotatef(fEarthRot, 0.0f, 1.0f, 0.0f);
+
+	glColor3ub(0, 0, 255);	//earth draw
+	glTranslatef(105.0f, 0.0f, 0.0f);
+	glutSolidSphere(15.0f, 15, 15);
+	glColor3ub(200, 200, 200);	//moon draw & rotation
+	glRotatef(fMoonRot, 0.0f, 1.0f, 0.0f);
+	glTranslatef(30.0f, 0.0f, 0.0f);
+	fMoonRot += 15.0f;
+	if (fMoonRot > 360.0f) { fMoonRot = 0.0f; }
+	glutSolidSphere(6.0f, 15, 15);
+
+	glPopMatrix();
+	fEarthRot += 5.0f;
+	if (fEarthRot > 360.0f) { fEarthRot = 0.0f; }
+	glutSwapBuffers();
+}
+
+int main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutCreateWindow("OpenGL Solar System");
+	glutReshapeFunc(ChangeSize);
+	glutDisplayFunc(RenderScene);
+
+	glutMainLoop();
 	return 0;
 }
